9775db4c0d8f9fc96ba5c06b889aab9b653ca01a
Will <williamrwilson@gmail.com>
2015-04-19 22:13:00 +0000

    add atom feed

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
 
   get 'store/index'
 
-  resources :products
+  resources :products do 
+    get :who_bought, on: :member
+  end
+    
 
   root :to => "store#index", as: "store"
 
